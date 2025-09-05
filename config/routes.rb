905378db7689c45e2946ca32c54136a5ec4a8657
Steve Jahns <s.t.jahns@gmail.com>
2025-09-05 17:34:23 -0600

    Add GraphQL gem and install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
